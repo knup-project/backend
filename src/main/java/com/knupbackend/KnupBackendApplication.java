dc14fa3a0a0b291f9c9709d955bb7f7bd3833bfb
hanseung-yeon <jumdo12@gmail.com>
2026-05-24 23:03:11 +0900

    first commit

---
+++ src/main/java/com/knupbackend/KnupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knupbackend/KnupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
